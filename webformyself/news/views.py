64da1e49bfcd86f5291c96978c4206b504ef6977
ilyshev <dilyshev@mac.com>
2020-08-21 13:00:06 -0400

    adding form add_news

--- webformyself/news/views.py
+++ webformyself/news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
+from .forms import NewsForm
 from .models import News, Category
 
 
@@ -22,3 +23,11 @@ def view_news(request, news_id):
     # news_item = News.objects.get(pk=news_id)
     news_item = get_object_or_404(News, pk=news_id)
     return render(request, 'news/view_news.html', {"news_item": news_item})
+
+
+def add_news(request):
+    if request.method == "POST":
+        pass
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
