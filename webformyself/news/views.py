cbb001565b00a708840da787e4eb5487bf50a813
ilyshev <dilyshev@mac.com>
2020-08-21 14:07:37 -0400

    working with forms.py

--- webformyself/news/views.py
+++ webformyself/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import NewsForm
 from .models import News, Category
@@ -27,7 +27,11 @@ def view_news(request, news_id):
 
 def add_news(request):
     if request.method == "POST":
-        pass
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
     else:
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})
